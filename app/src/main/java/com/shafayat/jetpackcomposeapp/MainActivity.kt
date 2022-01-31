7df6f51946ea5d451d15f2bc929028fc3a567a76
Shafayat <mindfreakicup33@gmail.com>
2022-01-31 21:58:07 +0600

    Compose initial commit

---
+++ app/src/main/java/com/shafayat/jetpackcomposeapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.shafayat.jetpackcomposeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = "Yeepee")
+                Spacer(modifier = Modifier.padding(top = 16.dp))
+                Button(onClick = { }) {
+                    Text(text = "Compose Button")
+                }
+            }
+        }
+    }
+}
