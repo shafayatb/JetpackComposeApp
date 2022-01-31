02a5db8eb0d61aad4c3df160b1d3aa1dc342b69e
Shafayat <mindfreakicup33@gmail.com>
2022-01-31 22:44:17 +0600

    Image, column and scroll

--- app/src/main/java/com/shafayat/jetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/shafayat/jetpackcomposeapp/MainActivity.kt
@@ -3,13 +3,24 @@ package com.shafayat.jetpackcomposeapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,12 +28,37 @@ class MainActivity : AppCompatActivity() {
 
         setContent {
             Column(
-                modifier = Modifier.padding(16.dp)
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+                    .fillMaxSize()
             ) {
-                Text(text = "Yeepee")
-                Spacer(modifier = Modifier.padding(top = 16.dp))
-                Button(onClick = { }) {
-                    Text(text = "Compose Button")
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = "",
+                    modifier = Modifier.height(300.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text(
+                        text = "Android Logo",
+                        style = TextStyle(
+                            fontSize = 17.sp
+                        )
+                    )
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(
+                        text = "$4.99",
+                        style = TextStyle(
+                            color= Color(0xFF85bb65),
+                            fontSize = 17.sp
+                        )
+                    )
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Button(onClick = { }) {
+                        Text(text = "Compose Button")
+                    }
                 }
             }
         }
